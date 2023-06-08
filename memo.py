71325e60338a8ff2762d449acf65dca74fe485f7
yasushi0843 <yamamotoyasushi1970@gmail.com>
2023-06-08 18:24:10 +0900

    update memo.py

--- memo.py
+++ memo.py
@@ -1,3 +1,4 @@
+# Version: 0.1.1
 import os
 import sys
 import argparse
@@ -7,10 +8,15 @@ from PIL.PngImagePlugin import PngInfo
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('png', nargs='*', help='png file')
 parser.add_argument('-p', '--python', action='store_true', help='show path of python executable binary')
+parser.add_argument('-l', '--link', action='store_true', help='show link strings for windows shortcut')
 args = parser.parse_args()
 
 if args.python:
     print(sys.executable)
+    sys.exit()
+
+if args.link:
+    print('"' + sys.executable + '" "' + os.path.abspath(__file__) + '"')
     sys.exit()
 
 if len(args.png) == 0:
